ae28c97ade5121a80919effd6b8dc4bf22e73581
Muhammed Caylak <m.recep.caylak@gmail.com>
2020-05-06 06:48:25 +0300

    connections completed

--- src/app/services/signal-r.service.ts
+++ src/app/services/signal-r.service.ts
@@ -1,6 +1,8 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import * as SignalR from '@aspnet/signalr';
 import {ChatUser} from '../shared/model/ChatUser';
+import SharedFunctions from "../shared/shared-functions";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +12,15 @@ export class SignalRService {
   userReceived = new EventEmitter<ChatUser>();
 
   private hubConnection: SignalR.HubConnection;
+  private sharedFunctions = new SharedFunctions(this.router);
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   public buildConnection = () => {
+    const username = this.sharedFunctions.readLocalStorage('username');
     this.hubConnection = new SignalR.HubConnectionBuilder()
-      .withUrl('http://localhost:9089/')
+      .withUrl('https://localhost:44322/chathub?username='+ username)
       .build();
   };
 
